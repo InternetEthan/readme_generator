f7041bb64d47e1f4880d05c30902f6fe6f232063
InternetEthan <internetethan@gmail.com>
2023-10-10 17:02:30 -0500

    initial setup; added packages and basic inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+    {
+        type:'input',
+        name: 'title',
+        message: 'Enter the title of your project',
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: 'What is the purpose of this project',
+    },
+    // additional information to be added later
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+}
 
 // Function call to initialize app
 init();

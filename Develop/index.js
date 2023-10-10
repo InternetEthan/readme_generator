d40d6d882333e9bb5295aeaa8727a3d5c7a23dcd
InternetEthan <internetethan@gmail.com>
2023-10-10 18:21:04 -0500

    added additional options to create a more complete README

--- Develop/index.js
+++ Develop/index.js
@@ -3,11 +3,30 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 
-const generateREADME = ({ title, description }) =>
+const generateREADME = ({ title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, githubUsername, githubEmail }) =>
   `# ${title}
 
 ## Description
-${description}`;
+${description}
+
+## Installation
+${installationInstructions}
+
+## Usage
+${usageInformation}
+
+## Contributing
+${contributionGuidelines}
+
+## Test Instructions
+${testInstructions}
+
+## Questions
+If you have any questions you can reach me via email or GitHub:
+[${githubUsername}](https://github.com/${githubUsername})
+Email: ${githubEmail}
+
+`;
 
 
 // array of questions for user input
@@ -16,18 +35,50 @@ inquirer
     {
       type: 'input',
       name: 'title',
-      message: 'Enter the title of your project',
+      message: 'Enter the title of your project?',
     },
     {
       type: 'input',
       name: 'description',
-      message: 'What is the purpose of this project',
+      message: 'What is the purpose of this project?',
     },
     {
       type: 'input',
       name: 'directoryPath',
       message: 'Enter the absolute path where you want to save the README file:',
     },
+    {
+      type: 'input',
+      name: 'installationInstructions',
+      message: 'Installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usageInformation',
+      message: 'Usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributionGuidelines',
+      message: 'Contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'testInstructions',
+      message: 'Test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub Username for the Questions section of the README:',
+    },
+    {
+      type: 'input',
+      name: 'githubEmail',
+      message: 'Enter your GitHub email for the Questions section of the README:',
+    },
+
+
     // additional information to be added later
   ])
   .then((answers) => {
